ac3fa2bde728ef945afb15255dcf469c059a2bd9
Dongwuk <donghae0414@naver.com>
2022-02-18 02:34:07 +0900

    create one public subnet, one private subnet

--- AwsStudy/tf_simple_vpc/main.tf
+++ AwsStudy/tf_simple_vpc/main.tf
@@ -5,10 +5,79 @@ resource "aws_vpc" "vpc-tf" {
   }
 }
 
+
+###
+### public subnet
+###
 resource "aws_subnet" "sbn-tf-public-2a" {
-  vpc_id = "${aws_vpc.vpc-tf.id}"
+  vpc_id     = aws_vpc.vpc-tf.id
   cidr_block = var.sbn_var_map["cidr_blocks"]["public-2a"]
+  availability_zone = var.sbn_var_map["az"]["public-2a"]
   tags = {
     Name = var.sbn_var_map["names"]["public-2a"]
   }
+}
+
+resource "aws_default_route_table" "rt-public" {
+  default_route_table_id = aws_vpc.vpc-tf.default_route_table_id
+  tags = {
+    Name = var.rt_public_name
+  }
+}
+
+resource "aws_route_table_association" "rtas-tf-public-2a" {
+  subnet_id = aws_subnet.sbn-tf-public-2a.id
+  route_table_id = aws_default_route_table.rt-public.id
+}
+
+resource "aws_internet_gateway" "igw-tf" {
+  vpc_id = aws_vpc.vpc-tf.id
+  tags = {
+    Name = var.igw_name
+  }
+}
+
+resource "aws_route" "route-public" {
+  route_table_id = aws_default_route_table.rt-public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.igw-tf.id
+}
+
+
+###
+### private subnet
+###
+resource "aws_subnet" "sbn-tf-private-2a" {
+  vpc_id     = aws_vpc.vpc-tf.id
+  cidr_block = var.sbn_var_map["cidr_blocks"]["private-2a"]
+  tags = {
+    Name = var.sbn_var_map["names"]["private-2a"]
+  }
+}
+
+resource "aws_route_table" "rt-tf-private" {
+  vpc_id = aws_vpc.vpc-tf.id
+  tags = {
+    Name = var.rt_private_name
+  }
+}
+
+resource "aws_route_table_association" "rtas-tf-private-2a" {
+  subnet_id = aws_subnet.sbn-tf-private-2a.id
+  route_table_id = aws_route_table.rt-tf-private.id
+}
+
+resource "aws_eip" "eip-nat" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "nat-tf" {
+  allocation_id = aws_eip.eip-nat.id
+  subnet_id = aws_subnet.sbn-tf-private-2a.id
+}
+
+resource "aws_route" "route-private" {
+  route_table_id = aws_route_table.rt-tf-private.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_nat_gateway.nat-tf.id
 }
--- AwsStudy/tf_simple_vpc/variables.tf
+++ AwsStudy/tf_simple_vpc/variables.tf
@@ -26,10 +26,35 @@ variable "sbn_var_map" {
     cidr_blocks = {
       public-2a = "10.0.0.0/28"
       public-2c = "10.0.0.16/28"
+      private-2a = "10.0.0.32/28"
+      private-2c = "10.0.0.48/28"
+    },
+    az = {
+      public-2a = "ap-northeast-2a"
+      public-2c = "ap-northeast-2c"
+      private-2a = "ap-northeast-2a"
+      private-2c = "ap-northeast-2c"
     },
     names = {
       public-2a = "sbn-tf-public-2a"
       public-2c = "sbn-tf-public-2c"
+      private-2a = "sbn-tf-private-2a"
+      private-2c = "sbn-tf-private-2a"
     }
   }
+}
+
+variable "igw_name" {
+  type = string
+  default = "igw-tf"
+}
+
+variable "rt_public_name" {
+  type = string
+  default = "rt-tf-public"
+}
+
+variable "rt_private_name" {
+  type = string
+  default = "rt-tf-private"
 }
